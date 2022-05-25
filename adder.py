209b1ae695390637ac959e264dfda6b6d65fee2e
ᴍᴜᴋᴇsʜ <100356452+Itz-mst-boy@users.noreply.github.com>
2022-05-25 10:15:21 +0530

    Update adder.py

--- adder.py
+++ adder.py
@@ -76,7 +76,7 @@ print('\n' + info + lg + ' Checking for banned accounts...' + rs)
 for a in accounts:
     phn = a[0]
     print(f'{plus}{grey} Checking {lg}{phn}')
-    clnt = TelegramClient(f'sessions/{phn}', 8088717, '7d1e0295ee1c2628f1933e9ffd2d8b78')
+    clnt = TelegramClient(f'sessions/{phn}', 11849799, '2bdcc0f0b3a04954d1cc1b11d5a1f669')
     clnt.connect()
     banned = []
     if not clnt.is_user_authorized():
@@ -167,7 +167,7 @@ adding_status = 0
 approx_members_count = 0
 for acc in to_use:
     stop = index + 60
-    c = TelegramClient(f'sessions/{acc[0]}', 8088717 , '7d1e0295ee1c2628f1933e9ffd2d8b78')
+    c = TelegramClient(f'sessions/{acc[0]}', 11849799 , '2bdcc0f0b3a04954d1cc1b11d5a1f669')
     print(f'{plus}{grey} User: {cy}{acc[0]}{lg} -- {cy}Starting session... ')
     c.start(acc[0])
     acc_name = c.get_me().first_name
